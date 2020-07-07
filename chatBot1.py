cd35a0c5a2559a6d66539faf2da7c3652eb27d00
oboy-1 <67874638+oboy-1@users.noreply.github.com>
2020-07-07 15:38:23 -0400

    Added ask/tell variability and added docstrings to functions.

--- chatBot1.py
+++ chatBot1.py
@@ -11,7 +11,12 @@ class image:
         self.special_question = special_question
         self.special_answer = special_answer
 
+
     def get_attribute_tag(self, number):
+        '''
+        This function returns the attribute type given a number from 0-4 inclusive
+        '''
+
         if number == 0:
             return 'title'
         elif number == 1:
@@ -23,7 +28,12 @@ class image:
         else:
             raise Exception('number out of range')
 
+
     def get_attribute(self, number):
+        '''
+        This function returns the attribute given a number from 0-4 inclusive
+        '''
+
         if number == 0:
             return self.title
         elif number == 1:
@@ -35,7 +45,12 @@ class image:
         else:
             raise Exception('get_attribute NUMBER OUT OF RANGE')
 
+
     def check_answer(self, obj, ans):
+        '''
+        This function returns a message based on ans relevance with obj as a answer key
+        '''
+
         if type(obj) != list:
             if ans.lower() in obj.lower():
                 return 'Yes, that is correct!'
@@ -47,9 +62,14 @@ class image:
             if ans.lower() in obj:
                 return 'Yes, that is correct!'
             else:
-                return 'Not really.... It\'s ' + str(obj)
+                return 'Not really.... It\'s ' + str(obj).strip('[').strip(']')
+
 
     def ask_question(self, attr):
+        '''
+        This function asks a question giving an attribute type and returns a sentence
+        '''
+
         if attr == 'title':
             return 'What does this picture show?'
         elif attr == 'location':
@@ -61,7 +81,13 @@ class image:
         else:
             raise Exception('ask_questions ATTR IS NOT VALID')
 
+
     def tell_something(self, attr):
+        '''
+        This function tells information given an attribute tag and uses info on the metadata of the image to return a
+        sentence
+        '''
+
         if attr == 'title':
             return 'This picture is showing ' + self.title
         elif attr == 'location':
@@ -69,11 +95,15 @@ class image:
         elif attr == 'people':
             return 'People in this photo are ' + str(self.people).strip('[').strip(']')
         elif attr == 'special_question':
-            return self.special_answer
+            pass
         else:
             raise Exception('ask_questions ATTR IS NOT VALID')
 
-image1 = image('Keerti\'s Wedding', 'New Jersey', ['Keerti', 'Alex', 'Babu Nanna', 'Peddamma'], 'What is Alex\'s brothers name', 'James')
+
+
+
+image1 = image('Keerti\'s Wedding', 'New Jersey', ['Keerti', 'Alex', 'Uncle', 'Aunt'],
+               'What is Alex\'s brothers name', 'James')
 
 attributeList = []
 while len(attributeList) < 4:
@@ -82,9 +112,15 @@ while len(attributeList) < 4:
     attribute = image1.get_attribute(attribute_index)
 
     if attribute not in attributeList:
-        print(image1.ask_question(attribute_tag))
-        answer = input('Enter your answer here --> ')
-        print(image1.check_answer(attribute, answer))
+        should_ask = random.randrange(0, 2, 1)
+        if should_ask:
+            print(image1.ask_question(attribute_tag))
+            answer = input('Enter your reply here --> ')
+            print(image1.check_answer(attribute, answer))
+        else:
+            if image1.tell_something(attribute_tag) != None:
+                print(image1.tell_something(attribute_tag))
+                answer = input('Enter your reply here --> ')
         attributeList.append(attribute)
     else:
         continue
